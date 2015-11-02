6e6d61b1d07d78a5394cfd0eaf94a3ab537fbb86
llaull <laul@llovem.eu>
2015-11-02 14:42:04 +0100

    avant bidouille

--- src/Domotique/bundle/ModuleBundle/Controller/EmplacementController.php
+++ src/Domotique/bundle/ModuleBundle/Controller/EmplacementController.php
@@ -25,8 +25,25 @@ class EmplacementController extends Controller
 
         $entities = $em->getRepository('DomotiquebundleModuleBundle:Emplacement')->findAll();
 
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $entities,
+            $this->get('request')->query->get('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+//        $entities=$entities->getQuery()->getResult();
+
+        $delete_forms  = array_map(
+            function($element){
+                return $this->createDeleteForm($element->getId());}
+            ,$entities->toArray()
+        );
+
+        die(var_dump($delete_forms));
+
         return $this->render('DomotiquebundleModuleBundle:Emplacement:index.html.twig', array(
-            'entities' => $entities,
+            'pagination' => $pagination,
+            'delete_forms'    => $delete_forms
         ));
     }
     /**
@@ -44,7 +61,7 @@ class EmplacementController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('admin_domotique_module_emplacement_show', array('id' => $entity->getId())));
+            return $this->redirect($this->generateUrl('admin_domotique_module_emplacement'));
         }
 
         return $this->render('DomotiquebundleModuleBundle:Emplacement:new.html.twig', array(
@@ -185,7 +202,7 @@ class EmplacementController extends Controller
      * Deletes a Emplacement entity.
      *
      */
-    public function deleteAction(Request $request, $id)
+   /* public function deleteAction(Request $request, $id)
     {
         $form = $this->createDeleteForm($id);
         $form->handleRequest($request);
@@ -203,7 +220,34 @@ class EmplacementController extends Controller
         }
 
         return $this->redirect($this->generateUrl('admin_domotique_module_emplacement'));
+    }*/
+
+
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $entity = $em->getRepository('DomotiquebundleModuleBundle:Emplacement')
+                ->find($id);
+            // this line might need to be changed to point to the proper repository
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Emplacement entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_domotique_module_emplacement'));
+        // this line might need to be changed to point to the proper
+        // post-delete route
     }
+
 
     /**
      * Creates a form to delete a Emplacement entity by id.
@@ -214,11 +258,17 @@ class EmplacementController extends Controller
      */
     private function createDeleteForm($id)
     {
-        return $this->createFormBuilder()
+
+        return $this->createFormBuilder(array('id' => $id))
+        ->add('id', 'hidden')
+        ->getForm()
+    ;
+
+        /*return $this->createFormBuilder()
             ->setAction($this->generateUrl('admin_domotique_module_emplacement_delete', array('id' => $id)))
             ->setMethod('DELETE')
             ->add('submit', 'submit', array('label' => 'Delete'))
             ->getForm()
-        ;
+        ;*/
     }
 }
