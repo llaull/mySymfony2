6837085eae1ade0ad5703b7509be26eafca60408
llaull <laul@llovem.eu>
2015-11-17 17:12:17 +0100

    Signed-off-by: llaull <laul@llovem.eu>

--- src/Commun/UserBundle/Entity/User.php
+++ src/Commun/UserBundle/Entity/User.php
@@ -7,21 +7,21 @@ use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
-* @ORM\Entity
-* @ORM\Table(name="commun__user")
-*/
+ * @ORM\Entity
+ * @ORM\Table(name="commun__user")
+ */
 class User extends BaseUser
 {
-   /**
-    * @ORM\Id
-    * @ORM\Column(type="integer")
-    * @ORM\GeneratedValue(strategy="AUTO")
-    */
-   protected $id;
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-   public function __construct()
-   {
-       parent::__construct();
-       // your own logic
-   }
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
