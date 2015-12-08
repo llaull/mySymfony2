1b177c682a6a52a31e50ad62298ca57a84cd0d35
llaull <laul@llovem.eu>
2015-12-08 15:20:37 +0100

    flux json de twitter : ok

--- src/CarnetsBundle/Controller/TwitterController.php
+++ src/CarnetsBundle/Controller/TwitterController.php
@@ -3,41 +3,85 @@
 namespace CarnetsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TwitterController extends Controller
 {
 
-    public function indexAction($count = 1)
+    public function indexAction()
     {
-        $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 
-        $oauth_access_token = $this->getParameter('oauth_access_token');
-        $oauth_access_token_secret = $this->getParameter('oauth_access_token_secret');
+        $token = $this->getParameter('oauth_access_token');
+        $token_secret = $this->getParameter('oauth_access_token_secret');
         $consumer_key = $this->getParameter('consumer_key');
         $consumer_secret = $this->getParameter('consumer_secret');
+        $twitter_account = $this->getParameter('twitter_account');
+        $tweet_limit = $this->getParameter('tweet_limit');
+        $ext_proxy = $this->getParameter('ext_proxy');
 
-        $oauth = array('count' => $count,
+        $host = 'api.twitter.com';
+        $method = 'GET';
+        $path = '/1.1/statuses/user_timeline.json'; // api call path
+
+        $query = array( // query parameters
+            'screen_name' => $twitter_account,
+            'count' => $tweet_limit
+        );
+
+        $oauth = array(
             'oauth_consumer_key' => $consumer_key,
-            'oauth_nonce' => time(),
-            'oauth_signature_method' => 'HMAC-SHA1',
-            'oauth_token' => $oauth_access_token,
+            'oauth_token' => $token,
+            'oauth_nonce' => (string)mt_rand(), // a stronger nonce is recommended
             'oauth_timestamp' => time(),
-            'oauth_version' => '1.0');
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_version' => '1.0'
+        );
 
-        $base_info = $this->buildBaseString($url, 'GET', $oauth);
-        $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
-        $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
-        $oauth['oauth_signature'] = $oauth_signature;
+        $oauth = array_map("rawurlencode", $oauth); // must be encoded before sorting
+        $query = array_map("rawurlencode", $query);
 
-// Make Requests
-        $header = array($this->buildAuthorizationHeader($oauth), 'Expect:');
-        $options = array(CURLOPT_HTTPHEADER => $header,
-            //CURLOPT_POSTFIELDS => $postfields,
+        $arr = array_merge($oauth, $query); // combine the values THEN sort
+
+        asort($arr); // secondary sort (value)
+        ksort($arr); // primary sort (key)
+
+        // http_build_query automatically encodes, but our parameters
+        // are already encoded, and must be by this point, so we undo
+        // the encoding step
+        $querystring = urldecode(http_build_query($arr, '', '&'));
+
+        $url = "https://$host$path";
+
+        // mash everything together for the text to hash
+        $base_string = $method . "&" . rawurlencode($url) . "&" . rawurlencode($querystring);
+
+        // same with the key
+        $key = rawurlencode($consumer_secret) . "&" . rawurlencode($token_secret);
+
+        // generate the hash
+        $signature = rawurlencode(base64_encode(hash_hmac('sha1', $base_string, $key, true)));
+
+        // this time we're using a normal GET query, and we're only encoding the query params
+        // (without the oauth params)
+        $url .= "?" . http_build_query($query);
+        $url = str_replace("&amp;", "&", $url); //Patch by @Frewuill
+
+        $oauth['oauth_signature'] = $signature; // don't want to abandon all that work!
+
+        // this is the full value of the Authorization line
+        $auth = "OAuth " . urldecode(http_build_query($oauth, '', ', '));
+
+        // if you're doing post, you need to skip the GET building above
+        // and instead supply query parameters to CURLOPT_POSTFIELDS
+        $options = array(CURLOPT_HTTPHEADER => array("Authorization: $auth"),
             CURLOPT_HEADER => false,
-            CURLOPT_URL => $url . '&count=' . $count,
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYPEER => false);
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_PROXY => $ext_proxy
+        );
 
+        // do our business
         $feed = curl_init();
         curl_setopt_array($feed, $options);
         $json = curl_exec($feed);
@@ -45,48 +89,6 @@ class TwitterController extends Controller
 
         $twitter_data = json_decode($json);
 
-//        return $twitter_data;
-        die(var_dump($twitter_data));
-    }
-
-    /**
-     * Private method to generate the base string used by cURL
-     *
-     * @param string $baseURI
-     * @param string $method
-     * @param array $params
-     *
-     * @return string Built base string
-     */
-    private function buildBaseString($baseURI, $method, $params)
-    {
-        $return = array();
-        ksort($params);
-
-        foreach ($params as $key => $value) {
-            $return[] = "$key=" . $value;
-        }
-
-        return $method . "&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $return));
-    }
-
-    /**
-     * Private method to generate authorization header used by cURL
-     *
-     * @param array $oauth Array of oauth data generated by buildOauth()
-     *
-     * @return string $return Header used by cURL for request
-     */
-    private function buildAuthorizationHeader($oauth)
-    {
-        $return = 'Authorization: OAuth ';
-        $values = array();
-
-        foreach ($oauth as $key => $value) {
-            $values[] = "$key=\"" . rawurlencode($value) . "\"";
-        }
-
-        $return .= implode(', ', $values);
-        return $return;
+        return new JsonResponse(array('tweets' => $twitter_data));
     }
 }
