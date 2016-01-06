4a8193b61db05c03b087dbd5d2b02e81f6cacbd5
llaull <laul@llovem.eu>
2016-01-06 12:43:47 +0100

    remove all file test

--- src/Commun/UserBundle/Tests/UserControllerTest.php
+++ src/Commun/UserBundle/Tests/UserControllerTest.php
@@ -14,10 +14,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class UserControllerTest extends WebTestCase
 {
 
-    public function testCompleteScenario($username = 'test', $password = 'test')
+    public function logIn($client, $username = 'test', $password = 'test')
     {
-
-        $client = static::createClient();
+//        $client = static::createClient();
 
         $crawler = $client->request('GET', '/login');
         $form = $crawler->selectButton('_submit')->form(array(
@@ -27,6 +26,42 @@ class UserControllerTest extends WebTestCase
         $client->submit($form);
 
         $this->assertTrue($client->getResponse()->isRedirect());
+    }
+
+    protected function createAuthorizedClient()
+    {
+        $client = static::createClient();
+        $container = $client->getContainer();
+
+        $session = $container->get('session');
+        /** @var $userManager \FOS\UserBundle\Doctrine\UserManager */
+        $userManager = $container->get('fos_user.user_manager');
+        /** @var $loginManager \FOS\UserBundle\Security\LoginManager */
+        $loginManager = $container->get('fos_user.security.login_manager');
+        $firewallName = $container->getParameter('fos_user.firewall_name');
+
+        $user = $userManager->findUserBy(array('username' => 'test'));
+        $loginManager->loginUser($firewallName, $user);
+
+        // save the login token into the session and put it in a cookie
+        $container->get('session')->set('_security_' . $firewallName,
+            serialize($container->get('security.context')->getToken()));
+        $container->get('session')->save();
+        $client->getCookieJar()->set(new Cookie($session->getName(), $session->getId()));
+
+        return $client;
+    }
+
+    public function testCompleteScenario()
+    {
+
+        $client = static::createClient();
+        $this->logIn($client);
+
+//        $crawler = $client->request('GET', "/admin/carnet/blog/article/");
+//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+//        $client->click($crawler->selectLink('Create a new entry')->link());
 
     }
 
