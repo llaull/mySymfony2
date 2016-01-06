0604ef0000809d90789d607ea0a07a0cabd9fa35
llaull <laul@llovem.eu>
2016-01-06 09:02:52 +0100

    c2v  - blog - rq correction majuscules dans la requet

--- src/CarnetsBundle/Repository/BlogRepository.php
+++ src/CarnetsBundle/Repository/BlogRepository.php
@@ -15,7 +15,7 @@ class BlogRepository extends EntityRepository
 
     public function findArticleWithInfo($em)
     {
-        $rq = "SELECT A.title AS articleTitre, A.slug, A.actived, A.image, A.publied, U.username, (SELECT count(*) FROM carnet2voyage__blogcomment WHERE arcticle_id = A.id) AS commentNb, Cat.title FROM carnet2voyage__blogarticle AS A JOIN carnet2voyage__blogcategory AS Cat ON Cat.id = A.category_id JOIN commun__user AS U ON U.id = A.autor_id";
+        $rq = "SELECT A.title AS articleTitre, A.slug, A.actived, A.image, A.publied, U.username, (SELECT count(*) FROM carnet2voyage__blogComment WHERE arcticle_id = A.id) AS commentNb, Cat.title FROM carnet2voyage__blogArticle AS A JOIN carnet2voyage__blogCategory AS Cat ON Cat.id = A.category_id JOIN commun__user AS U ON U.id = A.autor_id";
 
         $connection = $em->getConnection();
         $statement = $connection->prepare($rq);
@@ -27,7 +27,7 @@ class BlogRepository extends EntityRepository
 
     public function findCategoyWithInfo($em)
     {
-        $rq = "SELECT *,(SELECT count(*) FROM `carnet2voyage__blogarticle` WHERE category_id = C.id) AS articleNb FROM carnet2voyage__blogcategory AS C";
+        $rq = "SELECT *,(SELECT count(*) FROM `carnet2voyage__blogArticle` WHERE category_id = C.id) AS articleNb FROM carnet2voyage__blogCategory AS C";
 
         $connection = $em->getConnection();
         $statement = $connection->prepare($rq);
