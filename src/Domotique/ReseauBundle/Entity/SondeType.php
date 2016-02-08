5d227f0e0f85de45ceff69b7a741fad0a509d544
llaull <laul@llovem.eu>
2016-02-08 15:37:11 +0100

    Signed-off-by: llaull <laul@llovem.eu>

---
+++ src/Domotique/ReseauBundle/Entity/SondeType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Domotique\ReseauBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SondeType
+ *
+ * @ORM\Table(name="domotique__sonde_type")
+ * @ORM\Entity
+ */
+class SondeType
+{
+    public function __toString()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=90, nullable=false)
+     */
+    private $nom;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return SondeType
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
