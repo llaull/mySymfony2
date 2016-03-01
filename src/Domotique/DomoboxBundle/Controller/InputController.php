5be9fbfc930674e5a3cd7b51c2f22edd73a6dc81
llaull <laul@llovem.eu>
2016-03-01 16:14:01 +0100

    Signed-off-by: llaull <laul@llovem.eu>

--- src/Domotique/DomoboxBundle/Controller/InputController.php
+++ src/Domotique/DomoboxBundle/Controller/InputController.php
@@ -9,8 +9,45 @@ use Domotique\ReseauBundle\Entity\Module;
 
 class InputController extends Controller
 {
+
+    public function addFuxFakeAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+//        $sensorFluxAdd = new \DateTime(rand(-12, 12).' hour');
+        $sensorFluxAdd = new \DateTime();
+
+        $moduleId = 2;
+        $sensorId = 1;
+        $sensorTypeId = 2;
+        $sensorUnitId = 2;
+        $sensorValue = rand(20, 25);
+
+        $log = new Log();
+
+        $moduleX = $em->getRepository('DomotiqueReseauBundle:Module')->find($moduleId);
+        $sensorType = $em->getRepository('DomotiqueReseauBundle:SensorType')->find($sensorTypeId);
+        $sensorUnit = $em->getRepository('DomotiqueReseauBundle:SensorUnit')->find($sensorUnitId);
+
+        try {
+            $log->setModule($moduleX);
+            $log->setSensorId($sensorId);
+            $log->setSensorType($sensorType);
+            $log->setSensorUnit($sensorUnit);
+            $log->setSonsorValue($sensorValue);
+            $log->setCreated($sensorFluxAdd);
+            $em->persist($log);
+            $em->flush();
+        } catch (\Doctrine\ORM\EntityNotFoundException $e) {
+            error_log($e->getMessage());
+            return new JsonResponse(array('requete' => $e->getMessage()));
+        }
+
+        return new JsonResponse(array('requete' => "sucess"));
+    }
+
     public function addJsonAction()
     {
+
         $em = $this->getDoctrine()->getManager();
         $params = array();
         $logger = $this->get('logger');
@@ -40,12 +77,12 @@ class InputController extends Controller
             $logger->error("ok !");
             foreach ($params['sensors'] as $k => $v) {
                 $log = new Log();
-                $SondeType = $em->getRepository('DomotiqueReseauBundle:SensorType')->find($params['sensors'][$k]['sensor type Id']);
-                $SondeUnit = $em->getRepository('DomotiqueReseauBundle:SensorUnit')->find($params['sensors'][$k]['sensor unit Id']);
+                $sensorType = $em->getRepository('DomotiqueReseauBundle:SensorType')->find($params['sensors'][$k]['sensor type Id']);
+                $sensorUnit = $em->getRepository('DomotiqueReseauBundle:SensorUnit')->find($params['sensors'][$k]['sensor unit Id']);
                 $log->setModule($moduleX);
                 $log->setSensorId($params['sensors'][$k]['sensor Id']);
-                $log->setSensorType($SondeType);
-                $log->setSensorUnit($SondeUnit);
+                $log->setSensorType($sensorType);
+                $log->setSensorUnit($sensorUnit);
                 $log->setSonsorValue($params['sensors'][$k]['sensor value']);
                 $em->persist($log);
             }
--- src/Domotique/DomoboxBundle/Controller/OutputController.php
+++ src/Domotique/DomoboxBundle/Controller/OutputController.php
@@ -18,20 +18,73 @@ class OutputController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
         $entities = $this->getDoctrine()->getRepository('DomotiqueReseauBundle:Log');
-        $entities = $entities->getMoyenHourGroupByModule($em, 1, 2);
+        $entities = $entities->getMoyenHourGroupByModule($em, 2, 1);
 
         return new JsonResponse($entities);
     }
 
-    public function getCurrentDateAction(){
+    public function getCurrentDateAction()
+    {
         $now = new \DateTime();
-        $currentDate = $now->format('d-m-Y G:i:s');
+        $currentDate = $now->format('d-m-Y');
+        $currentDateFr = $now->format('d-m-Y');
+        $currentHour = $now->format('G:i:s');
 
-        $return = array("time" => $currentDate);
+        $return = array("date" => $currentDate, "hour" => $currentHour);
         return new JsonResponse($return);
     }
 
-    public function getCurrentTemp(){
+    public function getCurrentValueAction()
+    {
+
+        $rq = 'SELECT
+    l.id,
+    l.module_id,
+    l.created AS date,
+    l.sonsor_unit,
+    l.sensor_type,
+    l.sonsor_value,
+    m.name,
+    m.mac,
+    u.slug,
+    t.name AS nom_sonde,
+    E.name AS emplacement
+FROM
+    domotique__sensor_log AS l
+        INNER JOIN
+    (SELECT
+        module_id, sonsor_unit, sensor_type, MAX(created) AS max_temp
+    FROM
+        domotique__sensor_log
+    WHERE
+         created > CURDATE()
+        AND
+       -- created > SUBTIME(NOW(),"02:00:00")
+      --   AND
+      DATE_SUB(CURTIME(),INTERVAL 1 HOUR) >= created
+    GROUP BY module_id , sonsor_unit , sensor_type) AS b ON b.module_id = l.module_id
+        AND b.sonsor_unit = l.sonsor_unit
+        AND b.max_temp = l.created
+        LEFT JOIN
+    domotique__module AS m ON l.module_id = m.id
+        LEFT JOIN
+    domotique__sensor_unit AS u ON l.sonsor_unit = u.id
+        LEFT JOIN
+    domotique__sensor_type AS t ON l.sensor_type = t.id
+        LEFT JOIN
+    domotique__module_emplacement AS E ON m.emplacement_id = E.id
+WHERE
+    l.created > CURDATE()
+ORDER BY module_id , sensor_type , sonsor_unit';
+
+        $em = $this->getDoctrine()->getManager();
+        $connection = $em->getConnection();
+        $statement = $connection->prepare($rq);
+        $statement->execute();
+        $results = $statement->fetchAll();
+
+        return new JsonResponse($results);
+//        return $results;
 
     }
 }
